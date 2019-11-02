6d99923e30f6737d5640e7417b5403072b281ba8
Sebastian M <sebi.markus@googlemail.com>
2019-11-02 14:36:49 +0100

    Initial commit

---
+++ GccApplication1/main.cpp
@@ -0,0 +1,18 @@
+/*
+ * GccApplication1.cpp
+ *
+ * Created: 02.11.2019 14:34:55
+ * Author : Ness
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+

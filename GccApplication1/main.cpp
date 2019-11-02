f58172cae5d3346757cfa6d44e10b81f6e065fdf
Sebastian M <sebi.markus@googlemail.com>
2019-11-02 17:52:45 +0100

    test

--- GccApplication1/main.cpp
+++ GccApplication1/main.cpp
@@ -6,13 +6,31 @@
  */ 
 
 #include <avr/io.h>
+#include <avr/interrupt.h>
 
+ISR(INT0_vect)
+{
+	class LED
+	{
+		
+	public:
+	         
+	void init() {ddrB.bit0 = 1;}         
+	void on() {portB.bit0 = 1;}         
+	void off() {portB.bit0 = 0;} 
+	
+	};
+}
 
 int main(void)
 {
     /* Replace with your application code */
     while (1) 
     {
+		
+		
+		
     }
 }
+
 

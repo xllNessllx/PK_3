a057e0d8c3f30ac7afe07f22a1aafc426dd63be9
Sebastian M <sebi.markus@googlemail.com>
2019-11-20 13:12:59 +0100

    done

--- GccApplication1/main.cpp
+++ GccApplication1/main.cpp
@@ -11,6 +11,7 @@
 #define F_CPU 1000000UL
 #include <util/delay.h>
 
+void ledOff();
 void ledRed();
 void ledYellow();
 
@@ -30,6 +31,9 @@ void ledRed(){
 		if(~PINC & 0x02){
 			break;
 		}
+		if(~PINC & 0x01){
+			ledOff();
+		}
 	}
 }
 
@@ -41,6 +45,21 @@ void ledYellow(){
 		_delay_ms(200);
 		if(~PINC & 0x01){
 			break;
+		}
+		if(~PINC & 0x02){
+			ledOff();
+		}
+	}
+}
+
+void ledOff(){
+	while(1){
+		_delay_ms(200);
+		if(~PINC & 0x01){
+			ledRed();
+		}
+		if(~PINC & 0x02){
+			ledYellow();
 		}
 	}
 }
